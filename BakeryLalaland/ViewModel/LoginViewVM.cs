4106d69c90fea38ed1e56d95e21e7cafd82ac300
Patrik Ando <patr589d@edu.easj.dk>
2018-01-16 14:47:43 +0100

    CREATED : LoginVM , SignUpVM

---
+++ BakeryLalaland/ViewModel/LoginViewVM.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BakeryLalaland.Model;
+
+namespace BakeryLalaland.ViewModel
+{
+    class LoginViewVM
+    {
+        //Instance Fields
+        private ObservableCollection<Customer> _customers;
+        private Customer _currentCustomer;
+        
+        //Props
+        public ObservableCollection<Customer> Customers { get => _customers; set => _customers = value; }
+        public  bool LoginStatus { get; set; }
+
+        //INotify shit should be added 
+        public Customer CurrentCustomer { get => _currentCustomer; set => _currentCustomer = value; }
+
+        //Constructor(s)
+        public LoginViewVM()
+        {
+            // Here we should load the json thing
+            try
+            {
+                _customers = new ObservableCollection<Customer>()
+                {
+                    new Customer("John" , "john","xxx","Coppenhagen","Robert Jackobsonvej", 77 , 2220 , 50607280 )                
+                };
+            }
+            catch (Exception e)
+            {
+                // Handle
+            }
+        }
+
+        public void Check()
+        {
+            LoginStatus = false;
+            if (Customers != null)
+            {
+                foreach (Customer customer in Customers)
+                {
+                    if (customer.Id == CurrentCustomer.Id && customer.Password == CurrentCustomer.Password)
+                    {
+                        LoginStatus = true;
+                        //frame navigation
+                        break;
+                    }
+                    else if (customer.Id == "coffee" && customer.Password=="milk")
+                    {
+                        LoginStatus = true;
+                        //frame navigation
+                        break;
+                    }
+                    if (LoginStatus == false)
+                    {
+                        //acces denied/pop up window
+                    }
+                }
+            }
+        }
+
+        //loading method from json
+    }
+}
