f68cd57d58526ed4caeca8f1a7b43368ab1b0508
Diiiya <33103806+Diiiya@users.noreply.github.com>
2018-01-18 16:05:53 +0200

    GetItem updated

--- BakeryLalaland/Persistency/GetItem.cs
+++ BakeryLalaland/Persistency/GetItem.cs
@@ -43,7 +43,33 @@ namespace BakeryLalaland.Persistency
             return DrinksCatalog;
         }
 
+        //-------------------------Customers----------------
+        public ObservableCollection<Customer> CustomersCatalog { get; set; }
 
+        //saving into the file
+        public async Task SavetoJson(ObservableCollection<Customer> customers)
+        {
+            var localFolder = ApplicationData.Current.LocalFolder;
+            var jsonFile = await localFolder.CreateFileAsync("Customers.txt", CreationCollisionOption.ReplaceExisting);
+            var jsonSerializer = new DataContractJsonSerializer(typeof(ObservableCollection<Customer>));
+            using (var stream = await jsonFile.OpenStreamForWriteAsync())
+            {
+                jsonSerializer.WriteObject(stream, customers);
+            }
+        }
+
+        //retriving from the file
+        public async Task<ObservableCollection<Customer>> LoadFromJson()
+        {
+            var localFolder = ApplicationData.Current.LocalFolder;
+            var jsonFile = await localFolder.GetFileAsync("Customers.txt");
+            var jsonSerializer = new DataContractJsonSerializer(typeof(ObservableCollection<Customer>));
+            using (var stream = await jsonFile.OpenStreamForReadAsync())
+            {
+                CustomersCatalog = jsonSerializer.ReadObject(stream) as ObservableCollection<Customer>;
+            }
+            return CustomersCatalog;
+        }
 
     }
 }
